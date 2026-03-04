307c2c34c27dd327e4087ab0d1f066896f7a9bf5
blackop166179 <tah9216@icloud.com>
2026-03-04 13:52:16 -0800

    new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	modified:   GEIGER_PEDAL.cpp 	new file:   GEIGER_PEDAL.exe 	new file:   GEIGER_PEDAL.ilk 	new file:   GEIGER_PEDAL.obj 	new file:   GEIGER_PEDAL.pdb 	deleted:    Geiger_pedal.py 	new file:   vc140.pdb

--- GEIGER_PEDAL.cpp
+++ GEIGER_PEDAL.cpp
@@ -1,8 +1,17 @@
-#include <iostream>;
-#include <cmath>;
-#include <random>;
+#include <iostream>
+#include <cmath>
+#include <random>
 
-sdt::random_device rd;
-sdt::rd.max(1);
-sdt::rd.min(0);
 
+float Geiger(float lambda){
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    static std::uniform_real_distribution<float>dist(0.0f, 1.0f); 
+
+    float P = dist(gen);
+    float tic = -std::log(1.0f - P) / lambda;
+    return tic;
+}
+void main() {
+    std::cout << "Geiger(4.0f) = " << std::endl;
+}
